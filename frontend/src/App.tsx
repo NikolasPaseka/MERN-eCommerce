3ab5894786820e16d926de3c8e6e94b9e05b0a7a
nikolas_paseka <nikolas.paseka7@gmail.com>
2023-07-26 23:32:24 +0200

    List product page style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,46 @@
-import './App.css';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { sampleProducts } from './data';
 
 function App() {
-    return (
-        <div>
-            <header>TS Amazon</header>
-            <main>
-                <ul>
-                    {sampleProducts.map((product) => (
-                        <li key={product.slug}>
-                            <img
-                                src={product.image}
-                                alt={product.name}
-                                className="productImage"
-                            />
-                            <h2>{product.name}</h2>
-                            <p>${product.price}</p>
-                        </li>
-                    ))}
-                </ul>
-            </main>
-            <footer>All rights reserved</footer>
-        </div>
-    );
+	return (
+		<div className="d-flex flex-column vh-100">
+			<header>
+				<Navbar bg="dark" variant="dark" expand="lg">
+					<Container>
+						<Navbar.Brand>TSAmazon</Navbar.Brand>
+					</Container>
+					<Nav>
+						<a href="/cart" className="nav-link">
+							Cart
+						</a>
+						<a href="/signIn" className="nav-link">
+							Sign in
+						</a>
+					</Nav>
+				</Navbar>
+			</header>
+			<main>
+				<Container className="mt-3">
+					<Row>
+						{sampleProducts.map((product) => (
+							<Col key={product.slug} sm={6} md={4} lg={3}>
+								<img
+									src={product.image}
+									alt={product.name}
+									className="product-image"
+								/>
+								<h2>{product.name}</h2>
+								<p>${product.price}</p>
+							</Col>
+						))}
+					</Row>
+				</Container>
+			</main>
+			<footer>
+				<div className="text-center">All rights reserved</div>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
