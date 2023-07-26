1aa456b8d5ef0c6e81b758e48d10be4ef9970c21
nikolas_paseka <nikolas.paseka7@gmail.com>
2023-07-26 22:22:41 +0200

    list products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
 import './App.css';
+import { sampleProducts } from './data';
 
 function App() {
-  return (
-    <div>
-      <header>TS Amazon</header>
-      <main></main>
-      <footer>All rights reserved</footer>
-    </div>
-  );
+    return (
+        <div>
+            <header>TS Amazon</header>
+            <main>
+                <ul>
+                    {sampleProducts.map((product) => (
+                        <li key={product.slug}>
+                            <img
+                                src={product.image}
+                                alt={product.name}
+                                className="productImage"
+                            />
+                            <h2>{product.name}</h2>
+                            <p>${product.price}</p>
+                        </li>
+                    ))}
+                </ul>
+            </main>
+            <footer>All rights reserved</footer>
+        </div>
+    );
 }
 
 export default App;
