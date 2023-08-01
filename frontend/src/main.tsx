db0a5691e0e33b650649cde1f2cb8be78cf7c70b
nikolas_paseka <nikolas.paseka7@gmail.com>
2023-08-01 15:14:54 +0200

    React Context - dark mode switch

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.tsx';
 import './index.css';
@@ -14,6 +9,7 @@ import ProductPage from './pages/ProductPage.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StoreProvider } from './Store.tsx';
 
 // Navigation setup
 const router = createBrowserRouter(
@@ -21,8 +17,6 @@ const router = createBrowserRouter(
 		<Route path="/" element={<App />}>
 			<Route index={true} element={<HomePage />} />
 			<Route path="product/:slug" element={<ProductPage />} />
-			{/* <Route path="dashboard" element={<Dashboard />} /> */}
-			{/* ... etc. */}
 		</Route>
 	)
 );
@@ -31,11 +25,13 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<HelmetProvider>
-			<QueryClientProvider client={queryClient}>
-				<RouterProvider router={router} />
-				<ReactQueryDevtools initialIsOpen={false} />
-			</QueryClientProvider>
-		</HelmetProvider>
+		<StoreProvider>
+			<HelmetProvider>
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
+			</HelmetProvider>
+		</StoreProvider>
 	</React.StrictMode>
 );
